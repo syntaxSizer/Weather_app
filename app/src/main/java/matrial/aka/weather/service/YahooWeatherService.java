11f560d7e34c1e00c0519ec2a562c39ef5db07fc
syntaxSizer <kakuma4never@gmail.com>
2016-06-15 23:55:56 +0300

    add error handlers

--- app/src/main/java/matrial/aka/weather/service/YahooWeatherService.java
+++ app/src/main/java/matrial/aka/weather/service/YahooWeatherService.java
@@ -1,6 +1,12 @@
 package matrial.aka.weather.service;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by geckozila on 15/06/16.
@@ -8,6 +14,7 @@ import android.os.AsyncTask;
 public class YahooWeatherService {
     private  YahooWeatherCallBack callBack;
     private String location;
+    private Exception error;
 
     public YahooWeatherService(YahooWeatherCallBack callBack){
         this.callBack = callBack;
@@ -21,6 +28,19 @@ public class YahooWeatherService {
         new AsyncTask<String, Void, String>() {
             @Override
             protected String doInBackground(String... params) {
+
+                String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json", Uri.encode(YQL));
+                String YQL = String.format("select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\")"), location;
+                try {
+                    URL url = new URL(endpoint);
+                    URLConnection connection= url.openConnection();
+                } catch (MalformedURLException e) {
+                    error = e;
+                    return null;
+                } catch (IOException e) {
+                    error = e;
+                    return null;
+                }
                 return null;
             }
             @Override
