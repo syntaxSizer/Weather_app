941474fec905fd3332c291effc141410c46b7479
syntaxSizer <kakuma4never@gmail.com>
2016-06-15 22:53:29 +0300

    initialize views in main activity

--- app/src/main/java/matrial/aka/weather/MainActivity.java
+++ app/src/main/java/matrial/aka/weather/MainActivity.java
@@ -2,12 +2,21 @@ package matrial.aka.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView weatherImageView;
+    private TextView  temperture , conditionTextView, locationTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weatherImageView = (ImageView) findViewById(R.id.weather_icon_imageView);
+        temperture = (TextView)findViewById(R.id.temperture_text);
+        conditionTextView = (TextView)findViewById(R.id.condition_text);
+        locationTextView =(TextView)findViewById(R.id.location_TextView);
     }
 }
