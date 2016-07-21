20513daf591a1cf2cb8c4a802a7d94e0df519d92
syntaxSizer <kakuma4never@gmail.com>
2016-07-21 05:13:55 +0300

    add splash screen layout

---
+++ app/src/main/java/matrial/aka/weather/Splash.java
@@ -0,0 +1,9 @@
+package matrial.aka.weather;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by geckozila on 21/07/16.
+ */
+public class Splash extends AppCompatActivity {
+}
